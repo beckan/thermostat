698860bc9830598053ccdec4cfdaf55be1012dc5
Daniel <danielbackstrom@live.se>
2021-01-01 15:56:12 +0100

    Resolve watch-issue

--- src/index.js
+++ src/index.js
@@ -12,48 +12,44 @@ const getTemperatureSensor = require("./utils/get-temperature-sensor");
 const printMessage = require("./utils/print-message");
 
 const run = async () => {
-    try {
-        printMessage.splash(`Welcome to ${packageJSON.name} ${packageJSON.version}`);
+    printMessage.splash(`Welcome to ${packageJSON.name} ${packageJSON.version}`);
 
-        if (!doConfigFileExist()) {
-            createConfigFile();
-        }
-
-        printMessage.heading('Get temperature sensor');
-        const temperatureSensor = await getTemperatureSensor();
-
-        printMessage.success('[DONE]');
-
-        const gpioCooler = new Gpio(process.env.GPIO_COOLER, 'out');
-        const gpioHeater = new Gpio(process.env.GPIO_HEATER, 'out');
-
-        printMessage.heading('Starting API');
-
-        await startApi({
-            gpioCooler,
-            gpioHeater,
-            temperatureSensor
-        });
-
-        printMessage.success('[DONE]');
-
-        printMessage.heading('The thermostat is up and running');
-
-        temperatureSensor.on('change', (temperature) => {
-            temperatureWatch(temperature, gpioCooler, gpioHeater);
-        });
-
-        onApplicationExit(() => {
-            gpioCooler.writeSync(Gpio.LOW);
-            gpioHeater.writeSync(Gpio.LOW);
-        });
-    } catch (error) {
-        printMessage.error('[ERROR]');
-        console.log(error);
-        process.exit(1);
+    if (!doConfigFileExist()) {
+        createConfigFile();
     }
+
+    printMessage.heading('Get temperature sensor');
+    const temperatureSensor = await getTemperatureSensor();
+
+    printMessage.success('[DONE]');
+
+    const gpioCooler = new Gpio(process.env.GPIO_COOLER, 'out');
+    const gpioHeater = new Gpio(process.env.GPIO_HEATER, 'out');
+
+    printMessage.heading('Starting API');
+
+    await startApi({
+        gpioCooler,
+        gpioHeater,
+        temperatureSensor
+    });
+
+    printMessage.success('[DONE]');
+
+    printMessage.heading('The thermostat is up and running');
+
+    temperatureSensor.on('change', (temperature) => {
+        temperatureWatch(temperature, gpioCooler, gpioHeater);
+    });
+
+    onApplicationExit(() => {
+        gpioCooler.writeSync(Gpio.LOW);
+        gpioHeater.writeSync(Gpio.LOW);
+    });
 };
 
 run().catch((error) => {
-    console.error(error);
+    printMessage.error('[ERROR]');
+    console.log(error);
+    process.exit(1);
 });
