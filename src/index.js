6a5ddaea0b90938e712a4dda064130be9d322fe2
Daniel <danielbackstrom@live.se>
2021-01-01 12:09:58 +0100

    Log temperature

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const W1Temp = require('w1temp');
+
+const packageJSON = require('../package.json');
+const settings = require('../settings.json');
+
+const getTemperatureSensor = async () => {
+    const sensors = await W1Temp.getSensorsUids();
+    return await W1Temp.getSensor(sensors.pop(), true);
+}
+
+const temperatureWatch = (temperature) => {
+    console.log(temperature);
+};
+
+const run = async () => {
+    console.log(`Welcome to ${packageJSON.name} ${packageJSON.version}`);
+
+    console.log('\nGet temperature sensor');
+    const temperatureSensor = await getTemperatureSensor();
+
+    const temperatureTarget = settings.temperature;
+    const temperatureThreshold = settings.temperatureThreshold;
+
+    temperatureSensor.on('change', temperatureWatch);
+
+    console.log('Termostat is up and running!\n\n');
+};
+
+run().catch((error) => {
+    console.error(error);
+});
